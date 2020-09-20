114b9e1398adfa9974009af50771d0258090339f
Shivanirao2000 <shivanirao2000@gmail.com>
2020-09-20 12:39:16 +0530

    Updates

---
+++ precache-manifest.cd39233d18a9175ec6048c1ea56929c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c4697fb3fd266a046f25e0ae06f5e6",
+    "url": "/github.io/ToDoList-React-app/index.html"
+  },
+  {
+    "revision": "8fee10c0412d5ff18002",
+    "url": "/github.io/ToDoList-React-app/static/css/main.35ee64cd.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/github.io/ToDoList-React-app/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/ToDoList-React-app/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fee10c0412d5ff18002",
+    "url": "/github.io/ToDoList-React-app/static/js/main.6c258f1d.chunk.js"
+  },
+  {
+    "revision": "8a70fac0df6c73af9dfd",
+    "url": "/github.io/ToDoList-React-app/static/js/runtime-main.67b8479f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/ToDoList-React-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
